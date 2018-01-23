be662f5c8bf494f045a07e85750c101e185e9193
g.glechyan <g.glechyan@corp.mail.ru>
2018-01-23 16:42:02 +0300

    Всё работает и на чётных и на нечётных числах

--- Codewars/GetTheMiddleCharacter.py
+++ Codewars/GetTheMiddleCharacter.py
@@ -2,11 +2,29 @@
 # If the word's length is odd, return the middle character. 
 # If the word's length is even, return the middle 2 characters.
 
+import math
+
+
 # Получаем слово
 word = input("Введите слово: ")
-wordLength = len(word)
 
+# Вычисляем и печатаем длину
+wordLength = len(word)
+print(wordLength)
+
+# Проверяем, что длина правильная
 if wordLength == 0 or wordLength > 200:
     print("Ваше слово не подходит")
+    quit()
 
-print (wordLength)
+# Проверяем чётное ли число символов
+if wordLength % 2 == 0:
+    # Число символов чётное, поэтому выводим 2 символа
+    symbol1 = word[int(wordLength/2) - 1]
+    symbol2 = word[int(wordLength/2)]
+    print(symbol1, symbol2)
+
+else:
+    # Число символов нечётное, поэтому выводим слово по середине
+    symbol1 = word[int(math.floor(wordLength/2))]
+    print(symbol1)
