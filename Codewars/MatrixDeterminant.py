e770e66db5f05eea3aa40afe7d5283c0c1c190e2
g.glechyan <g.glechyan@corp.mail.ru>
2018-02-12 10:24:11 +0300

    Описал альтернативное решение
    Подсмотрено в готовых решениях, но именно так я и хотел сделать

--- Codewars/MatrixDeterminant.py
+++ Codewars/MatrixDeterminant.py
@@ -1,23 +1,9 @@
 # Write a function that accepts a square matrix (n x n 2D array) and returns the determinant of the matrix.
-
-# How to take the determinant of a matrix -- it is simplest to start with the smallest cases: 
-# A 1x1 matrix |a| has determinant a. A 2x2 matrix [[a, b], [c, d]] or
-# |a b|
-# |c d|
-# has determinant ad - bc.
-
-# The determinant of an n x n sized matrix is calculated by reducing the problem to the calculation of the determinants of n n-1 x n-1 matrices. 
-# For the 3x3 case, [[a, b, c], [d, e, f], [g, h, i]] or
-# |a b c|
-# |d e f|
-# |g h i|
-# the determinant is: a * det(a_minor) - b * det(b_minor) + c * det(c_minor) where det(a_minor) refers to taking the determinant of the 2x2 matrix created by crossing out the row and column in which the element a occurs, or
-# |e f|
-# |h i|
-
 # Note the alternation of signs.
 # The determinant of larger matrices are calculated analogously, e.g. if M is a 4x4 matrix with first row [a, b, c, d], det(M) = a * det(a_minor) - b * det(b_minor) + c * det(c_minor) - d * det(d_minor)
 
+
+# Более правильное, но читерское решение
 import numpy as np
 
 def determinant(matrix):
@@ -25,3 +11,35 @@ def determinant(matrix):
     return np.linalg.det(array)
 
 print(determinant ([[2,5,3], [1,-2,-1], [1, 3, 4]]))
+
+# # Менее правильное, но самостоятельное решение
+# # Функция для уменьшения размерности массива
+# def sub_determinant(matrix, i):
+#     sub_matrix = []
+#     # Ходим по массиву
+#     for j in range(1,len(matrix)):
+#         # Берём все элемент до i и элементы после i
+#         sub_matrix.append(matrix[j][:i] + matrix[j][i+1:])
+#     return sub_matrix
+    
+# def determinant(matrix):
+#     # Если размерность матрицы нулевая — возвращаем ноль
+#     if len(matrix) == 0:
+#         return 0
+#     # Если размерность матрицы 1 — возвращаем единственное число
+#     elif len(matrix) == 1:
+#         return matrix[0][0]
+#     # Если размерность матрицы 2 — считаем определетель
+#     elif len(matrix) == 2:
+#         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
+#     else:
+#         det = 0
+#         # Ходим по массиву
+#         for i in range(0,len(matrix)):
+#             # Смотрим на чётность и нечётность i (волшебство побитового "и": https://wiki.python.org/moin/BitwiseOperators) 
+#             # Плюс в дело вступает рекурсия — мы будем уменьшать размерность массива, пока она не станет 2*2
+#             if 1 == i & 1:
+#                 det = det - matrix[0][i] * determinant(sub_determinant(matrix,i))
+#             else:
+#                 det = det + matrix[0][i] * determinant(sub_determinant(matrix,i))
+#         return det
